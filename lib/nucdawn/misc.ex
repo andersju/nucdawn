7a24ff23270d19a156152a02f872c01af9808bf1
Anders Jensen-Urstad <anders@unix.se>
2017-09-25 21:54:48 +0200

    Don't use Timex unnecessarily

--- lib/nucdawn/misc.ex
+++ lib/nucdawn/misc.ex
@@ -13,12 +13,12 @@ defmodule Nucdawn.Misc do
     current_year = Date.utc_today.year()
     current_year_ccc_date = Date.from_iso8601!("#{current_year}-12-27")
 
-    case Timex.diff(current_year_ccc_date, Date.utc_today)  do
-      d when d > 0 -> reply "Next Congress in #{Timex.diff(current_year_ccc_date, Date.utc_today, :days)} days!"
+    case Date.diff(current_year_ccc_date, Date.utc_today)  do
+      d when d > 0 -> reply "Next Congress in #{Date.diff(current_year_ccc_date, Date.utc_today)} days!"
       d when d >= -3 -> reply "Congress is happening right now! Grab a Tschunk, #{message.user.nick}!"
       d when d <= -4 ->
         next_year_ccc_date = Date.from_iso8601!("#{current_year+1}-12-27")
-        reply "Next Congress in #{Timex.diff(next_year_ccc_date, Date.utc_today, :days)} days!"
+        reply "Next Congress in #{Date.diff(next_year_ccc_date, Date.utc_today)} days!"
     end
   end
 end
